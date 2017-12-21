79ee6eb8f227bca8defec1a474706c6f192015ec
Ajit Gosavi <ajitgosavi.2007@gmail.com>
2017-12-21 11:36:52 +0530

    added comments

--- SciKitBasic.py
+++ SciKitBasic.py
@@ -1,9 +1,16 @@
 
 from sklearn import tree
 
-
+# 0 is plane fruit 
+# 1 is bumpy fruit
 features = [[130,0],[160,1],[170,1],[120,0],[180,1]]
+
+# 0 is apple
+# 1 is orange
 label = [0,1,1,0,1]
+
 t = tree.DecisionTreeClassifier()
 t = t.fit(features,label)
+
+# You can test your prediction here
 print t.predict([[110,0]])
